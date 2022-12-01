227478da18c96e94b6bdd1ae1dda006862af5cbf
IrynaMe <irinam7a@gmail.com>
2022-12-01 16:26:28 +0100

    grazie page

--- resources/views/front/grazie.blade.php
+++ resources/views/front/grazie.blade.php
@@ -1,6 +1,6 @@
 @extends('front.layout')
 @section('content')
-    <div class="products-breadcrumb">
+    {{-- <div class="products-breadcrumb">
         <div class="container-fluid">
             <ul>
                 <li><i class="fa fa-home" aria-hidden="true"></i>
@@ -10,13 +10,32 @@
                 <li>Thank you!</li>
             </ul>
         </div>
-    </div>
+    </div> --}}
 
-    <div class="container" style="text-align:center">
-        <img src="{{ 'tema/images/thanks1.png' }}" alt="" width=40%">
+    {{--     <div class="container" style="text-align:center">
+        <img src="{{ 'tema/images/thanks1.png' }}" alt="" width="70%">
         <br>
         <a href="/"><button class="btn btn-success btn-lg" style="margin-top:20px;margin-bottom:20px">Return to
                 homepage</button></a>
         <br>
+    </div> --}}
+
+    <div class="thank">
+
     </div>
+    <div class="bg-text-thank">
+        <h2 style="font-size:50px">Thank you!</h2>
+        <br>
+
+
+        <h3>Check your email to see the order details</h3>
+        <br>
+        <p>Have a good time!</p>
+        <div>
+
+        </div>
+    </div>
+    <a class="bg-button-thank" href="/"><button class="btn btn-success btn-lg"
+            style="margin-top:20px;margin-bottom:20px">Return to
+            homepage</button></a>
 @endsection
